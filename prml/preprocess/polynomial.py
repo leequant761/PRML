d1438cd549e3de8c76ccde366dafa04d2556df3a
leequant761 <leequant761@gmail.com>
2020-01-04 11:45:07 +0900

    코드분석

--- prml/preprocess/polynomial.py
+++ prml/preprocess/polynomial.py
@@ -39,19 +39,25 @@ class PolynomialFeature(object):
 
         Parameters
         ----------
-        x : (sample_size, n) ndarray
+        x : (sample_size, p) ndarray
             input array
 
         Returns
         -------
-        output : (sample_size, 1 + nC1 + ... + nCd) ndarray
+        output : (sample_size, 1 + pC1 + ... + pCd) ndarray
             polynomial features
         """
         if x.ndim == 1:
-            x = x[:, None]
-        x_t = x.transpose()
-        features = [np.ones(len(x))]
+            x = x[:, None] # N x 1
+        x_t = x.transpose() # p x N
+        features = [np.ones(len(x))] # N개의 1
         for degree in range(1, self.degree + 1):
-            for items in itertools.combinations_with_replacement(x_t, degree):
-                features.append(functools.reduce(lambda x, y: x * y, items))
+            for items in itertools.combinations_with_replacement(x_t, degree): # items : 각 변수(item) 조합 (N 벡터 degree개) 튜플
+                features.append(functools.reduce(lambda x, y: x * y, items)) # 곱하기 예를들어 x1 x3 x5가 선택됐으면 x1 * x3 * x5 벡터 나옴
         return np.asarray(features).transpose()
+
+# 테스트 용
+if __name__=='__main__':
+    poly = PolynomialFeature(3)
+    x = np.arange(100).reshape(25, 4) # 25 x 4
+    poly.transform(x)
